70f551a8ac9d149bef438816e908f5e0686cf41a
ThienLK1 <koankem0901@gmail.com>
2022-04-18 11:04:29 +0700

    fix flow basic

--- contracts/minter/src/contract.rs
+++ contracts/minter/src/contract.rs
@@ -1,3 +1,5 @@
+#[cfg(not(feature = "library"))]
+use cosmwasm_std::entry_point;
 use cosmwasm_std::{
     to_binary, Addr, Binary, CosmosMsg, Deps, DepsMut, Empty, Env, MessageInfo, Order, Reply,
     ReplyOn, Response, StdResult, SubMsg, WasmMsg,
